67ad4f9f673045848efe8cba3a9bc3ea956c2f51
Md Tobibur Rohman <wpdevelopment20@gmail.com>
2022-05-10 22:08:56 +0600

    fillter add

--- redux-store-management/src/Components/Header/Header.jsx
+++ redux-store-management/src/Components/Header/Header.jsx
@@ -9,8 +9,8 @@ const Header = () => {
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Link to="/">Home</Link>
-            <Link to="/shop">Shop</Link>
+            <Link className="nav-menu" to="/">Home</Link>
+            <Link className="nav-menu" to="/shop">Shop</Link>
           </Nav>
         </Container>
       </Navbar>
--- redux-store-management/src/Components/Shop/Shop.jsx
+++ redux-store-management/src/Components/Shop/Shop.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Container, Col, Row, Tab, Tabs } from 'react-bootstrap'
-
+import Single from '../Products/Single'
+import moment from 'moment';
 const Shop = () => {
     const products = useSelector((state) => state.products.products[0])
+    //const lastDay = moment(launch_date_local).subtract(1, 'weeks').endOf('week').format('dddd')
+    // const LastMonth = { moment(launch_date_local).subtract(1, 'months').endOf('month').format('MMMM') }
+    // const LastYear = { moment(launch_date_local).subtract(1, 'year').endOf('year').format('YYYY') }
+
+    const launchSuccess = products?.filter((product) => product.launch_success === true)
+    const launchFailure = products?.filter((product) => product.launch_success === false)
+    const upcoming = products?.filter((product) => product.upcoming === true)
+
     console.log('Products: ', products)
+    console.log('launchSuccess: ', launchSuccess)
+    console.log('launchFailure: ', launchFailure)
     return (
         <Container fluid>
             <Container>
@@ -14,13 +25,46 @@ const Shop = () => {
                     </Col>
                 </Row>
                 <Row>
-                    <Col>
-                        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
+                    <Col md={12} sm={12} lg={12}>
+                        <Tabs defaultActiveKey="home" id="uncontrolled-tab-example" className="mb-5">
                             <Tab eventKey="home" title="All">
+                                <Row>
+                                    {
+                                        products?.map((item, i) => <Single key={i} item={item}></Single>)
+                                    }
+                                </Row>
                                 <h1>First Item</h1>
                             </Tab>
-                            <Tab eventKey="profile" title="Last Days">
-                                <h1>Second Item</h1>
+                            <Tab eventKey="lastDays" title="Last Days">
+                                <Row>
+                                    {
+                                        products?.map((item, i) => <Single key={i} item={item}></Single>)
+                                    }
+                                </Row>
+                            </Tab>
+                            <Tab eventKey="failure" title="Launch Failure">
+                                <h4 >Launch Failure Mission: {launchFailure.length}</h4>
+                                <Row className="mt-2">
+                                    {
+                                        launchFailure?.map((item, i) => <Single key={i} item={item}></Single>)
+                                    }
+                                </Row>
+                            </Tab>
+                            <Tab eventKey="success" title="Launch Success">
+                                <Row>
+                                    <h4>Launch Success Mission: {launchSuccess.length}</h4>
+                                    {
+                                        launchSuccess?.map((item, i) => <Single key={i} item={item}></Single>)
+                                    }
+                                </Row>
+                            </Tab>
+                            <Tab eventKey="upcoming" title="Upcoming">
+                                <Row>
+                                    <h4>Upcoming Mission: {upcoming.length}</h4>
+                                    {
+                                        upcoming?.map((item, i) => <Single key={i} item={item}></Single>)
+                                    }
+                                </Row>
                             </Tab>
                         </Tabs>
                     </Col>
